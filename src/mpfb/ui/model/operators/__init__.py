a2c4b77831437bbdd6659cdf71dde7f36047d902
Code-Explode-Studios <154757972+Code-Explode-Studios@users.noreply.github.com>
2025-04-24 16:59:25 +0100

    Randomize race operator added (not yet working) __init__.py

--- src/mpfb/ui/model/operators/__init__.py
+++ src/mpfb/ui/model/operators/__init__.py
@@ -6,8 +6,12 @@ _LOG.trace("initializing model operators module")
 
 from .refithuman import MPFB_OT_RefitHumanOperator
 from .prunehuman import MPFB_OT_PruneHumanOperator
+#from .randomize_race import MPFB_OT_Randomize_Race_Operator
+
 
 __all__ = [
     "MPFB_OT_RefitHumanOperator",
     "MPFB_OT_PruneHumanOperator"
+    #"MPFB_OT_Randomize_Race_Operator"
+    #when randomized race operator is enabled whole plug-in crashes
 ]
