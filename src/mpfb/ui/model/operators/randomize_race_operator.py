5b9d2c42b2948ada60386f2aa278f54f868441e7
Code-Explode-Studios <154757972+Code-Explode-Studios@users.noreply.github.com>
2025-04-24 16:57:40 +0100

    Race Randomization Operator (not working yet)

---
+++ src/mpfb/ui/model/operators/randomize_race_operator.py
@@ -0,0 +1,20 @@
+﻿import bpy
+from ....services import LogService
+from ....services import HumanService
+from ....services import ObjectService
+from .... import ClassManager
+
+_LOG = LogService.get_logger("model.randomizerace")
+
+class MPFB_OT_Randomize_Race_Operator(bpy.types.Operator): 
+    """Randomize character's race"""
+    bl_idname = "mpfb.randomize_race"
+    bl_label = "Randomize Race"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):                
+        _LOG.info("Randomizing race")
+        self.report({'INFO'}, "Race randomized!")
+        return {'FINISHED'}
+
+ClassManager.add_class(MPFB_OT_Randomize_Race_Operator)
